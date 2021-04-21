d87b02b91a08bc4d36f618ec2bb732e9c93d2843
ABrar Jahin <ajahin@iu.edu>
2021-04-21 19:33:28 -0400

    Hash Table Implemented

--- FinalProject.py
+++ FinalProject.py
@@ -1,12 +1,53 @@
-#from BST import User
-#from dir import Dir
 import sys          #For Command Line Argument
-#from HashTable import *
-import HashTable
+import io           #Read File
+from HashTable import HashTable
 import BST
 import SplayTree
 import SkipList
 import RBTree
 
-selector = sys.argv[1]
-inputFileNameInSameDir = sys.argv[2]
+def getInputFromFile(inputFileNameInSameDir: str):
+    data = []
+    with open(inputFileNameInSameDir) as f:
+        for line in [line.rstrip() for line in f]:
+            splittedData = line.replace('\t', ' ').split(" ", 1)
+            data.append((int(splittedData[0]), splittedData[1].strip())) 
+    return data
+
+def execute(selector: int, inputFileNameInSameDir: str):
+    selector = selector or 0
+    data = getInputFromFile(inputFileNameInSameDir or "input1.txt")
+    if selector==0: #HashTable
+        dataStructure = HashTable()
+        executeWithData(dataStructure, data)
+        print(dataStructure)
+    elif selector == 1: #BST
+        print('BST')
+    elif selector == 2: #BST
+        print('SplayTree')
+    elif selector == 3: #BST
+        print("SkipList")
+    elif selector == 4: #BST
+        print("RBTree")
+
+def executeWithData(modelObject, data):
+    for (choice, key) in data:
+        if choice == 0:#delete
+            modelObject.delete(key)
+        elif choice == 1:
+            if modelObject.isKeyExist(key):    # Search if exist
+                print("Searched and found key - " + key)
+            else:                       # Not found, so insert the data
+                modelObject.insert(key)
+
+def main():
+    if len(sys.argv)>1:
+        selector = sys.argv[1] or 0
+        inputFileNameInSameDir = sys.argv[2] or "input1.txt"
+    else:
+        selector = 0
+        inputFileNameInSameDir = "input1.txt"
+    return execute(selector,inputFileNameInSameDir)
+
+if __name__ == "__main__":
+    main()
--- HashTable.py
+++ HashTable.py
@@ -1,1 +1,45 @@
-print("HashTable Imported")
+class HashTable:
+    # default constructor
+    def __init__(self, maxNoOfElement = 100):
+        self.currentSize = 0
+        self.maxSize = maxNoOfElement
+        self.hashData = [None for _ in range(maxNoOfElement)]
+
+    # a method for printing data members
+    def __repr__(self) -> str:
+        return "Size : " + str(self.currentSize) + "/" + str(self.maxSize)
+
+    def __str__(self) -> str:
+        return ', '.join(str(e) for e in self.hashData if e is not None)
+
+    def getHashValue(self, keyString: str) -> int:
+        currentVal = 0
+        for val in [ord(ch) for ch in keyString]:
+            currentVal = currentVal*26 + val
+        return currentVal % self.maxSize
+
+    def isKeyExist(self, key: str) -> bool:
+        return self.hashData[self.getHashValue(key)] is not None
+
+    def search(self, key: str) -> bool:
+        return self.hashData[key]
+
+    def insert(self, key: str) -> str:
+        if self.isKeyExist(key):
+            #raise Exception("Sorry, Data Already Exists, Need overwrite")
+            print("Key Already Exists")
+            self.currentSize -= 1
+        self.hashData[self.getHashValue(key)] = key
+        self.currentSize += 1
+
+    def delete(self, key: str) -> None:
+        if not self.isKeyExist(key):
+            #raise Exception("Sorry, Data Not Exists")
+            print("Key Not Found")
+            self.currentSize += 1
+        self.hashData[self.getHashValue(key)] = None
+        self.currentSize -= 1
+
+    #def insert(data):  #If dynamic size
+    #    hash_key = Hashing(keyvalue)
+    #    Hashtable[hash_key].append(value)
