a03928adfd0b3df7880604a13d463ac68935d671
ABrar Jahin <ajahin@iu.edu>
2021-04-24 18:00:43 -0400

    RB Tree Update

--- RBTreeNode.py
+++ RBTreeNode.py
@@ -1,9 +1,12 @@
 from TreeNode import TreeNode
+from enum import Enum
+
+class Color(Enum):
+    RED = 1
+    BLACK = 0
 
 class RBTreeNode(TreeNode):
-    color = ""
-
-    def __init__(self, nodeParent="black", nodeColor="black", val=0, left=None, right=None):  
+    def __init__(self, val=0, nodeParent=None, nodeColor="black", left=None, right=None):  
         TreeNode.__init__(self, val, left, right)
-        self.color = nodeColor
+        self.color = Color.BLACK
         self.parent = nodeParent
