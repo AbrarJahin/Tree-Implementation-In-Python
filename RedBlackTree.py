553041c4e5c5bb8e676d891b9fd87071d186b212
ABrar Jahin <ajahin@iu.edu>
2021-04-26 12:37:48 -0400

    RB tree Pushed

--- RedBlackTree.py
+++ RedBlackTree.py
@@ -30,81 +30,81 @@ class RedBlackTree():
 		return self.search(key) is not None
 
 	# Node deletion
-	def delete(self, item: str) -> None:
-		self.deleteNodeHelper(self.root, item)
+	def delete(self, key: str) -> None:
+		self.deleteNodeHelper(self.root, key)
 	def deleteNodeHelper(self, node: RBTreeNode, key: str) -> None:
-		z = self.TRootNull
+		zNode = self.TRootNull
 		while node != self.TRootNull:
-			if node.val == key: z = node
+			if node.val == key: zNode = node
 			if node.val <= key: node = node.right
 			else: node = node.left
-		if z == self.TRootNull: return #Key Not Found
-		y = z
-		colorYPrev = y.color
-		if z.left == self.TRootNull:
-			x = z.right
-			self.rbTransplant(z, z.right)
-		elif (z.right == self.TRootNull):
-			x = z.left
-			self.rbTransplant(z, z.left)
+		if zNode == self.TRootNull: return #Key Not Found
+		yNode = zNode
+		colorYPrev = yNode.color
+		if zNode.left == self.TRootNull:
+			xNode = zNode.right
+			self.rbTransplant(zNode, zNode.right)
+		elif (zNode.right == self.TRootNull):
+			xNode = zNode.left
+			self.rbTransplant(zNode, zNode.left)
 		else:
-			y = self.findMinimum(z.right)
-			colorYPrev, x = y.color, y.right
-			if y.parent == z: x.parent = y
+			yNode = self.findMinimum(zNode.right)
+			colorYPrev, xNode = yNode.color, yNode.right
+			if yNode.parent == zNode: xNode.parent = yNode
 			else:
-				self.rbTransplant(y, y.right)
-				y.right = z.right
-				y.right.parent = y
-			self.rbTransplant(z, y)
-			y.left = z.left
-			y.left.parent, y.color = y, z.color
-		if colorYPrev == Color.BLACK: self.deleteFixupSubroutine(x)
+				self.rbTransplant(yNode, yNode.right)
+				yNode.right = zNode.right
+				yNode.right.parent = yNode
+			self.rbTransplant(zNode, yNode)
+			yNode.left = zNode.left
+			yNode.left.parent, yNode.color = yNode, zNode.color
+		if colorYPrev == Color.BLACK: self.deleteFixupSubroutine(xNode)
 
 	# Balancing the tree after deletion
-	def deleteFixupSubroutine(self, x: RBTreeNode) -> None:
-		while x != self.root and x.color == Color.BLACK:
-			if x == x.parent.left:
-				s = x.parent.right
-				if s.color == Color.RED:
-					s.color, x.parent.color = Color.BLACK, Color.RED
-					self.leftRotate(x.parent)
-					s = x.parent.right
-				if s.left.color == Color.BLACK and s.right.color == Color.BLACK:
-					s.color, x = Color.RED, x.parent
+	def deleteFixupSubroutine(self, xNode: RBTreeNode) -> None:
+		while xNode != self.root and xNode.color == Color.BLACK:
+			if xNode == xNode.parent.left:
+				sNode = xNode.parent.right
+				if sNode.color == Color.RED:
+					sNode.color, xNode.parent.color = Color.BLACK, Color.RED
+					self.leftRotate(xNode.parent)
+					sNode = xNode.parent.right
+				if sNode.left.color == Color.BLACK and sNode.right.color == Color.BLACK:
+					sNode.color, xNode = Color.RED, xNode.parent
 				else:
-					if s.right.color == Color.BLACK:
-						s.left.color = Color.BLACK
-						s.color = Color.RED
-						self.rightRotate(s)
-						s = x.parent.right
-					s.color, s.right.color, x.parent.color = x.parent.color, Color.BLACK, Color.BLACK
-					self.leftRotate(x.parent)
-					x = self.root
+					if sNode.right.color == Color.BLACK:
+						sNode.left.color = Color.BLACK
+						sNode.color = Color.RED
+						self.rightRotate(sNode)
+						sNode = xNode.parent.right
+					sNode.color, sNode.right.color, xNode.parent.color = xNode.parent.color, Color.BLACK, Color.BLACK
+					self.leftRotate(xNode.parent)
+					xNode = self.root
 			else:
-				s = x.parent.left
-				if s.color == Color.RED:
-					s.color, x.parent.color = Color.BLACK, Color.RED
-					self.rightRotate(x.parent)
-					s = x.parent.left
-				if s.right.color == Color.BLACK and s.right.color == Color.BLACK:
-					s.color = Color.RED
-					x = x.parent
+				sNode = xNode.parent.left
+				if sNode.color == Color.RED:
+					sNode.color, xNode.parent.color = Color.BLACK, Color.RED
+					self.rightRotate(xNode.parent)
+					sNode = xNode.parent.left
+				if sNode.right.color == Color.BLACK and sNode.right.color == Color.BLACK:
+					sNode.color = Color.RED
+					xNode = xNode.parent
 				else:
-					if s.left.color == Color.BLACK:
-						s.right.color, s.color = Color.BLACK, Color.RED
-						self.leftRotate(s)
-						s = x.parent.left
-					s.color = x.parent.color
-					x.parent.color, s.left.color = Color.BLACK, Color.BLACK
-					self.rightRotate(x.parent)
-					x = self.root
-		x.color = Color.BLACK
+					if sNode.left.color == Color.BLACK:
+						sNode.right.color, sNode.color = Color.BLACK, Color.RED
+						self.leftRotate(sNode)
+						sNode = xNode.parent.left
+					sNode.color = xNode.parent.color
+					xNode.parent.color, sNode.left.color = Color.BLACK, Color.BLACK
+					self.rightRotate(xNode.parent)
+					xNode = self.root
+		xNode.color = Color.BLACK
 
-	def rbTransplant(self, u: RBTreeNode, v: RBTreeNode) -> None:
-		if u.parent == None: self.root = v
-		elif u == u.parent.left: u.parent.left = v
-		else: u.parent.right = v
-		v.parent = u.parent
+	def rbTransplant(self, firstNode: RBTreeNode, secondNode: RBTreeNode) -> None:
+		if firstNode.parent == None: self.root = secondNode
+		elif firstNode == firstNode.parent.left: firstNode.parent.left = secondNode
+		else: firstNode.parent.right = secondNode
+		secondNode.parent = firstNode.parent
 
 	# Node insert
 	def insert(self, key: str) -> None:
@@ -112,16 +112,16 @@ class RedBlackTree():
 		node.left = self.TRootNull
 		node.right = self.TRootNull
 		node.color = Color.RED
-		y = None
-		x = self.root
-		while x != self.TRootNull:
-			y = x
-			if node.val < x.val: x = x.left
-			else: x = x.right
-		node.parent = y
-		if y == None: self.root = node
-		elif node.val < y.val: y.left = node
-		else: y.right = node
+		yNode = None
+		xNode = self.root
+		while xNode != self.TRootNull:
+			yNode = xNode
+			if node.val < xNode.val: xNode = xNode.left
+			else: xNode = xNode.right
+		node.parent = yNode
+		if yNode == None: self.root = node
+		elif node.val < yNode.val: yNode.left = node
+		else: yNode.right = node
 		if node.parent == None:
 			node.color = Color.BLACK
 			return
@@ -129,69 +129,66 @@ class RedBlackTree():
 		self.insertFixupSubroutine(node)
 
 	# Balance the tree after insertion
-	def insertFixupSubroutine(self, k: str) -> None:
-		while k.parent.color == Color.RED:
-			if k.parent == k.parent.parent.right:
-				u = k.parent.parent.left
-				if u.color == Color.RED:
-					u.color = Color.BLACK
-					k.parent.color = Color.BLACK
-					k.parent.parent.color = Color.RED
-					k = k.parent.parent
+	def insertFixupSubroutine(self, node: RBTreeNode) -> None:
+		while node.parent.color == Color.RED:
+			if node.parent == node.parent.parent.right:
+				sibling = node.parent.parent.left
+				if sibling.color == Color.RED:
+					sibling.color = Color.BLACK
+					node.parent.color = Color.BLACK
+					node.parent.parent.color = Color.RED
+					node = node.parent.parent
 				else:
-					if k == k.parent.left:
-						k = k.parent
-						self.rightRotate(k)
-					k.parent.color = Color.BLACK
-					k.parent.parent.color = Color.RED
-					self.leftRotate(k.parent.parent)
+					if node == node.parent.left:
+						node = node.parent
+						self.rightRotate(node)
+					node.parent.color = Color.BLACK
+					node.parent.parent.color = Color.RED
+					self.leftRotate(node.parent.parent)
 			else:
-				u = k.parent.parent.right
-
-				if u.color == Color.RED:
-					u.color = Color.BLACK
-					k.parent.color = Color.BLACK
-					k.parent.parent.color = Color.RED
-					k = k.parent.parent
+				sibling = node.parent.parent.right
+				if sibling.color == Color.RED:
+					sibling.color = Color.BLACK
+					node.parent.color = Color.BLACK
+					node.parent.parent.color = Color.RED
+					node = node.parent.parent
 				else:
-					if k == k.parent.right:
-						k = k.parent
-						self.leftRotate(k)
-					k.parent.color = Color.BLACK
-					k.parent.parent.color = Color.RED
-					self.rightRotate(k.parent.parent)
-			if k == self.root:
+					if node == node.parent.right:
+						node = node.parent
+						self.leftRotate(node)
+					node.parent.color = Color.BLACK
+					node.parent.parent.color = Color.RED
+					self.rightRotate(node.parent.parent)
+			if node == self.root:
 				break
 		self.root.color = Color.BLACK
 
 	def findMinimum(self, node: RBTreeNode) -> RBTreeNode:
-		while node.left != self.TRootNull:
-			node = node.left
+		while node.left != self.TRootNull: node = node.left
 		return node
 
 	def findMaximum(self, node: RBTreeNode) -> RBTreeNode:
-		while node.right != self.TRootNull:
-			node = node.right
+		while node.right != self.TRootNull: node = node.right
 		return node
 
-	def leftRotate(self, x: RBTreeNode) -> None:
-		y = x.right
-		x.right = y.left
-		if y.left != self.TRootNull: y.left.parent = x
-		y.parent = x.parent
-		if x.parent == None: self.root = y
-		elif x == x.parent.left: x.parent.left = y
-		else: x.parent.right = y
-		y.left = x
-		x.parent = y
+	def leftRotate(self, xNode: RBTreeNode) -> None:
+		yNode = xNode.right
+		xNode.right = yNode.left
+		if yNode.left != self.TRootNull: yNode.left.parent = xNode
+		yNode.parent = xNode.parent
+		if xNode.parent == None: self.root = yNode
+		elif xNode == xNode.parent.left: xNode.parent.left = yNode
+		else: xNode.parent.right = yNode
+		yNode.left = xNode
+		xNode.parent = yNode
 
-	def rightRotate(self, x: RBTreeNode) -> None:
-		y = x.left
-		x.left = y.right
-		if y.right != self.TRootNull: y.right.parent = x
-		y.parent = x.parent
-		if x.parent == None: self.root = y
-		elif x == x.parent.right: x.parent.right = y
-		else: x.parent.left = y
-		y.right = x
-		x.parent = y
+	def rightRotate(self, xNode: RBTreeNode) -> None:
+		yNode = xNode.left
+		xNode.left = yNode.right
+		if yNode.right != self.TRootNull: yNode.right.parent = xNode
+		yNode.parent = xNode.parent
+		if xNode.parent == None: self.root = yNode
+		elif xNode == xNode.parent.right: xNode.parent.right = yNode
+		else: xNode.parent.left = yNode
+		yNode.right = xNode
+		xNode.parent = yNode
